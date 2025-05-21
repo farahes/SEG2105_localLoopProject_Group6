d8c38510f74bd38edade518aba3cad1bc29f7b57
derikthiessen <derikt03@live.com>
2025-05-21 19:21:39 -0400

    new: class for Ogranizer implementation

---
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/Organizer.java
@@ -0,0 +1,9 @@
+public class Organizer extends User {
+    public Organizer() {
+        super();
+    }
+
+    public Organizer(String userID, String firstName, String lastName, String username, String email, String role) {
+        super(userID, firstName, lastName, username, email, role);
+    }
+}
