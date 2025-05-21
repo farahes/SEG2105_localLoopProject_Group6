03ae351dd362cd6f46e17c6795f1acc4b35b593e
derikthiessen <derikt03@live.com>
2025-05-21 19:22:58 -0400

    new: class for Participant implementation

---
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/Participant.java
@@ -0,0 +1,9 @@
+public class Participant extends User {
+    public Participant() {
+        super();
+    }
+
+    public Participant(String userID, String firstName, String lastName, String username, String email, String role) {
+        super(userID, firstName, lastName, username, email, role);
+    }
+}
