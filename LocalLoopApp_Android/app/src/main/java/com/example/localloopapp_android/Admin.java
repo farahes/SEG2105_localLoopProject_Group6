1665972f4d7d01898d2848ae88e7e1a0d1c0ac25
derikthiessen <derikt03@live.com>
2025-05-21 19:16:40 -0400

    new: class for Admin users

---
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/Admin.java
@@ -0,0 +1,10 @@
+public class Admin extends User {
+
+    public Admin() {
+        super();
+    }
+
+    public Admin(String userID, String firstName, String lastName, String username, String email, String role) {
+        super(userID, firstName, lastName, username, email, role);
+    }
+}
