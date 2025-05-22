6fb8c24506947d4cb8bbceb6d528421cdd2a81c1
derikthiessen <derikt03@live.com>
2025-05-21 20:26:37 -0400

    fix: add regex pattern for name pattern matching

--- LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/InputValidator.java
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/InputValidator.java
@@ -2,12 +2,15 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
 
+    private static final Pattern NAME_PATTERN = Pattern.compile(
+            "^[A-Za-z -]{1,}$"
+    );
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
     );
 
     public static boolean isValidName(String name) {
-        return name != null && name.matches("^[A-Za-z -]+$");
+        return name != null && NAME_PATTERN.matcher(name).matches();
     }
 
     public static boolean isValidEmail(String email) {
@@ -19,6 +22,6 @@ public class InputValidator {
     }
 
     public static boolean isValidPassword(String password) {
-        return password != null && password.length() >= 8; // Can adjust this line as needed for minimum password length
+        return password != null && password.length() >= 8; // Can adjust this line as needed for minimum password length or enforce regex for password complexity
     }
 }
