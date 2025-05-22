2b0d48f924df6a9583b0738c3480753a30c239f7
derikthiessen <derikt03@live.com>
2025-05-21 20:02:49 -0400

    new: method for validating emails

--- LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/InputValidator.java
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/InputValidator.java
@@ -1,8 +1,16 @@
+import java.util.regex.Pattern;
+
 public class InputValidator {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
+    );
 
     public static boolean isValidName(String name) {
         return name != null && name.matches("^[A-Za-z -]+$");
     }
 
-
+    public static boolean isValidEmail(String email) {
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
+    }
 }
