e6af88abfa5e8fec46cc67d208c151a29d0d0963
derikthiessen <derikt03@live.com>
2025-05-24 15:26:09 -0400

    new: phone number field

--- LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/User.java
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/User.java
@@ -6,16 +6,18 @@ public abstract class User {
     protected String lastName;
     protected String username;
     protected String email;
+    protected String phoneNumber;
     protected String role;
 
     public User() {}
 
-    public User(String userID, String firstName, String lastName, String username, String email, String role) {
+    public User(String userID, String firstName, String lastName, String username, String email, String phoneNumber, String role) {
         this.userID = userID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.email = email;
+        this.phoneNumber = phoneNumber;
         this.role = role;
     }
 
@@ -36,6 +38,8 @@ public abstract class User {
     public String getEmail() {
         return this.email;
     }
+
+    public String getPhoneNumber() { return this.phoneNumber; }
     public String getRole() {
         return this.role;
     }
@@ -53,6 +57,8 @@ public abstract class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
     public void setRole(String role) {
         this.role = role;
     }
