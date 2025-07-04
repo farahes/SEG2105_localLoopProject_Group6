f9d54b870f9844cecc08cc2f18f76901817335c5
farahes <farah@elsiss.com>
2025-07-04 08:46:59 -0400

    Added Manage Events Page ( shows upcoming events and event history )

---
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/activities/dashboard_activities/ManageEventsActivity.java
@@ -0,0 +1,13 @@
+package com.example.localloopapp_android.activities.dashboard_activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.localloopapp_android.R;
+
+public class ManageEventsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_events); // Make sure this matches your layout file
+    }
+}
