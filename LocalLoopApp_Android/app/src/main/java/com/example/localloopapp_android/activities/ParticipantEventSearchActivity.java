50dbc36306fd53ae878f1cebafca7b256ebf0a80
Giovanni Abbruzzo <gabbr085@uottawa.ca>
2025-07-06 11:38:04 -0400

    changed packaging to fix error

--- LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/activities/ParticipantEventSearchActivity.java
+++ LocalLoopApp_Android/app/src/main/java/com/example/localloopapp_android/activities/ParticipantEventSearchActivity.java
@@ -1,4 +1,4 @@
-package com.example.localloopapp_android.activities.participant;
+package com.example.localloopapp_android.activities;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
